496a1ad3c3aa4de55c89740c92d7d4dc099aa8e9
William Belle <william.belle@gmail.com>
2020-01-31 21:31:33 +0100

    Init setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+  name="elide-text",
+  version="0.0.1",
+  url="https://github.com/williambelle/elide-text",
+  author="William Belle",
+  author_email="william.belle@gmail.com",
+  description="A very simple text truncating function",
+  long_description=open('README.md').read(),
+  packages=setuptools.find_packages(),
+  install_requires=[],
+  zip_safe=False
+)
